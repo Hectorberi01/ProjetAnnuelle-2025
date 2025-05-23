2e31e66d75d740401f3bb70611241cd0582219b4
Hector <hector.adjakpa@gmail.com>
2025-05-23 07:54:56 +0200

    Update server.ts
    correction dans api-gateway

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -2,6 +2,8 @@ import app from './app';
 import * as env from 'dotenv';
 env.config();
 
-app.listen(process.env.PORT, () => {
-    console.log(`API Gateway is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`API Gateway is running on port ${port}`);
 });
